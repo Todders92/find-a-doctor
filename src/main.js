66ca85371245aa95fe916c3276ce0e42b998d562
Student <example@example.com>
2020-02-14 14:13:48 -0800

    add phone number data to search query

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         $("#output").text(`error in handling request`);
       } else if (response.data.length>0) {
         response.data.forEach(function(response){
-          $("ul#results").append(`<li>${response.profile.first_name} ${response.profile.last_name} ${response.practices[0].visit_address.city} ${response.practices[0].visit_address.state} ${response.practices[0].visit_address.street} ${response.practices[0].visit_address.zip} </li>`);
+          $("ul#results").append(`<li>${response.profile.first_name} ${response.profile.last_name} ${response.practices[0].visit_address.city} ${response.practices[0].visit_address.state} ${response.practices[0].visit_address.street} ${response.practices[0].visit_address.zip} ${"phone number:"} ${response.practices[0].phones[0].number} </li>`);
         });
       }
     }
