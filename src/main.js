9e72700b204b5bcd8d642adb039f6f6a2af4299b
Todd Walraven <cod4247@yahoo.com>
2020-02-23 12:48:08 -0800

    parse api for availible website information if provided

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         $("#errorText").text(`No matches found, try another name`);
       } else if (response.data.length>0) {
         response.data.forEach(function(response){
-          $("ul#results").append(`<li>${response.profile.first_name} ${response.profile.last_name} ${response.practices[0].visit_address.city} ${response.practices[0].visit_address.state} ${response.practices[0].visit_address.street} ${response.practices[0].visit_address.zip} ${"Phone number:"} ${response.practices[0].phones[0].number} ${"accepts new patients:"} ${response.practices[0].accepts_new_patients}</li>`);
+          $("ul#results").append(`<li>${response.profile.first_name} ${response.profile.last_name} ${response.practices[0].visit_address.city} ${response.practices[0].visit_address.state} ${response.practices[0].visit_address.street} ${response.practices[0].visit_address.zip} ${"Phone number:"} ${response.practices[0].phones[0].number} ${"accepts new patients:"} ${response.practices[0].accepts_new_patients} ${"additional contact: (blank if none provided)"} ${response.practices[0].website}</li>`);
         });
       }
     }
@@ -39,7 +39,7 @@ $(document).ready(function() {
         $("#errorSymptom").text(`No matches found, try inputting a different ailement`);
       } else if (answer.data.length>0) {
         answer.data.forEach(function(answer){
-          $("ul#symptomResults").append(`<li>${answer.profile.first_name} ${answer.profile.last_name} ${answer.practices[0].visit_address.city} ${answer.practices[0].visit_address.state} ${answer.practices[0].visit_address.street} ${answer.practices[0].visit_address.zip} ${"Phone number:"} ${answer.practices[0].phones[0].number} ${"accepts new patients:"} ${answer.practices[0].accepts_new_patients} </li>`);
+          $("ul#symptomResults").append(`<li>${answer.profile.first_name} ${answer.profile.last_name} ${answer.practices[0].visit_address.city} ${answer.practices[0].visit_address.state} ${answer.practices[0].visit_address.street} ${answer.practices[0].visit_address.zip} ${"Phone number:"} ${answer.practices[0].phones[0].number} ${"accepts new patients:"} ${answer.practices[0].accepts_new_patients} ${"additional contact: (blank if none provided)"} ${answer.practices[0].website}</li>`);
         });
       }
     }
